76df22a402794571af22f1a9362475da732afcd6
chriskyong22 <55605854+chriskyong22@users.noreply.github.com>
2020-10-22 15:35:55 -0400

    Cleaned up code to represent Test.java better

--- src/sample/back/Logic.java
+++ src/sample/back/Logic.java
@@ -23,8 +23,8 @@ public class Logic {
         }
         double value = alphabeta(map, this.depth, Integer.MIN_VALUE, Integer.MAX_VALUE, true, heuristicSelected);
         System.out.println("The value is: " + value);
-        map.printMap();
-        System.out.println("\n\n\n\n");
+        //map.printMap();
+        //System.out.println("\n\n\n\n");
         move(map.getCell(bestMove.getOrigin().getRow(), bestMove.getOrigin().getCol()),  map.getCell(bestMove.getGoal().getRow(), bestMove.getGoal().getCol()));
         map.printMap();
     }
